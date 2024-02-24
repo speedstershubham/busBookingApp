3ed057f009c7061b774f5438d7fca82864519fe1
shubhamDevVegavid <shubham.chowksay@vegavid.com>
2024-02-24 20:42:00 +0530

    add style in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,43 @@
-import { Button } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import { AppBar, Toolbar, Typography, Button } from '@mui/material'
 
 const Header: React.FC = () => {
     const navigate = useNavigate()
     return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <Button
-                            onClick={() => navigate('/booking')}
-                            color="inherit"
-                        >
-                            Booking
-                        </Button>
-                    </li>
-                    <li>
-                        <Button
-                            onClick={() => navigate('/Dashboard')}
-                            color="inherit"
-                        >
-                            Dashboard
-                        </Button>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" style={{ flexGrow: 1 }}>
+                    Your Website Name
+                </Typography>
+                <nav>
+                    <ul
+                        style={{
+                            listStyle: 'none',
+                            margin: 0,
+                            padding: 0,
+                            display: 'flex',
+                        }}
+                    >
+                        <li style={{ margin: '0 10px' }}>
+                            <Button
+                                onClick={() => navigate('/booking')}
+                                color="inherit"
+                            >
+                                Booking
+                            </Button>
+                        </li>
+                        <li style={{ margin: '0 10px' }}>
+                            <Button
+                                onClick={() => navigate('/dashboard')}
+                                color="inherit"
+                            >
+                                Dashboard
+                            </Button>
+                        </li>
+                    </ul>
+                </nav>
+            </Toolbar>
+        </AppBar>
     )
 }
 
