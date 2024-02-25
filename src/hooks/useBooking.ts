d8e9d15ca97883ef6d4dbcfef8ca52ae78dbc1f1
shubhamDevVegavid <shubham.chowksay@vegavid.com>
2024-02-25 13:18:07 +0530

    add more seat

--- src/hooks/useBooking.ts
+++ src/hooks/useBooking.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import IFormData from '../types/IFormData'
 import useSeats from './useSeat'
 import DeckTypes from '../components/Bus/DeckTypes'
